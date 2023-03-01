c7fa69c4fcee9eb5f92b02bbd0a6fd60a02cd24c
Aravind <aaravind281096@gmail.com>
2023-03-01 22:14:36 +0530

    add token

--- test.rb
+++ test.rb
@@ -1,1 +1,14 @@
-puts "this is the data from file"
+class Test
+    def initialize(git_token, fr_token)
+        @git_token = git_token
+        @fr_token = fr_token
+        puts @git_token
+        puts @fr_token
+    end
+end
+
+if __FILE__ ==$0
+    github_token = ARGV[0].strip
+    freshrelease_token = ARGV[1].strip
+    Test.new(github_token, freshrelease_token) 
+end
